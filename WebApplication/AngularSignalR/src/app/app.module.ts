eb420f84f735ee2dfb9c5a940850182d46a73af8
Marcin <marcin.kowalczyk@cassioli.com.pl>
2021-03-25 11:23:04 +0100

    adding modal

--- WebApplication/AngularSignalR/src/app/app.module.ts
+++ WebApplication/AngularSignalR/src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
+import { EmployeeDetailComponent } from './employee/employee-detail/employee-detail.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
-    EmployeeEditComponent
+    EmployeeEditComponent,
+    EmployeeDetailComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
