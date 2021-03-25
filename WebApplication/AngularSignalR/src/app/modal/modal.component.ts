7cefba56e26cf6cb9d7c29875e81f127c6046522
Marcin <marcinkow1619@gmail.com>
2021-03-25 22:53:34 +0100

    Notification

--- WebApplication/AngularSignalR/src/app/modal/modal.component.ts
+++ WebApplication/AngularSignalR/src/app/modal/modal.component.ts
@@ -1,17 +1,52 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { ModalService } from './modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.less'],
+  encapsulation: ViewEncapsulation.None 
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
 
-  @Input() id
+  @Input() id: string;
+  private element: any;     
   
-  constructor() { }
+  constructor(private modalService: ModalService, private el: ElementRef) {
+    this.element = el.nativeElement;
+   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if(!this.id) {
+      console.error('modal must have id');
+      return;
+    }
+
+    document.body.appendChild(this.element);  
+
+    this.element.addEventListener('click', el => {
+      if(el.target.className === 'app-modal') {
+        this.close();
+      }});
+      
+      this.modalService.add(this);
+  }
+
+  ngOnDestroy(): void {
+    this.modalService.remove(this.id);
+    this.element.remove();
+  }
+
+  open(): void {
+    this.element.style.display = 'block';
+    document.body.classList.add('app-modal-open');
+  }
+
+  close(): void {
+    this.element.style.display = 'none';
+    document.body.classList.remove('app-modal-open');
   }
 
 }
+
+
